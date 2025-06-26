a768e654f8ee15d1ebdab4ccc6e176f28b75dafb
KarnOllie <oliver@karn-geo.co.uk>
2025-06-26 08:16:13 +0100

    4

---
+++ auth.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  // Add logic to send credentials to backend here
+  alert('Logging in...');
+});
